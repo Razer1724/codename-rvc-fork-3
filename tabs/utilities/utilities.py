7a9d3ca7b9df4a9f5f0ff1cdf7884e6a30e0ba2c
Razer1724 <184171368+Razer1724@users.noreply.github.com>
2025-01-29 22:31:40 -0600

    Update

--- tabs/utilities/utilities.py
+++ tabs/utilities/utilities.py
@@ -9,6 +9,7 @@ sys.path.append(now_dir)
 from tabs.utilities.processing.processing import processing_tab
 from tabs.utilities.analyzer.analyzer import analyzer_tab
 from tabs.utilities.f0_extractor.f0_extractor import f0_extractor_tab
+from tabs.utilities.uvcp_creator.uvcp_creator import uvcp_creator_tab
 
 from assets.i18n.i18n import I18nAuto
 
@@ -29,3 +30,6 @@ def utilities_tab():
 
     with gr.TabItem(i18n("Audio Analyzer")):
         analyzer_tab()
+
+    with gr.TabItem(i18n("UVCP Creator")):
+        uvcp_creator_tab()  
